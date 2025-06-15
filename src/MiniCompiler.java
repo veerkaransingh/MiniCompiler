262ef241f6c7a97ac66a489d2549211c4e5101e9
Karan Singh <veerkaransinghmgm@gmail.com>
2025-06-14 23:26:09 -0400

    Initial commit - MiniCompiler

--- src/MiniCompiler.java
+++ src/MiniCompiler.java
@@ -1,5 +1,13 @@
+import java.util.regex.*;
+import java.util.*;
+
 public class MiniCompiler {
+
+    static class Token {
+        String type, value;
+    }
     public static void main(String[] args) {
         System.out.println("mini compiler");
     }
 }
+
