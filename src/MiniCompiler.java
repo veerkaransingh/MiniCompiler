c1ee66556e5149ec613d45316e2e89b637fabd0e
Karan Singh <veerkaransinghmgm@gmail.com>
2025-07-10 22:16:28 -0400

    Initial commit - MiniCompiler

--- src/MiniCompiler.java
+++ src/MiniCompiler.java
@@ -24,7 +24,7 @@ public class MiniCompiler {
         // Token types and their regex patterns
         String[][] patterns = {
                 {"Real", "[0-9]*\\.[0-9]+"},   // real numbers like 12.34
-                {"Integer", "[0-9]+"},
+                {"Integer", "[0-9]+"},         // Integer numbers like 56
                 {"Var_name", "[a-zA-Z_][a-zA-Z0-9_]*"},
                 {"Operator", "[+\\-*/^]"},
                 {"Assignment", ":="},
