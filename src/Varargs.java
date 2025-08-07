263b9f9c330528778a1b50cd4229083ea5341aa3
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-06 21:30:23 -0400

    Initial commit - MiniCompiler

--- src/Varargs.java
+++ src/Varargs.java
@@ -14,5 +14,6 @@ public class Varargs {
         System.out.println("weelcome to varagrs tutorial");
         System.out.println(" the sum of a and b is: "+ sum(3,4));
         System.out.println("the sum of 3, 2, 12 and 23 is: " + sum(3,23,132,233));
+        System.out.println(" the sum of 2 ,3 1 ,1 ,2 is: "+ sum(3, 3,  2, 2, 2, 2,2));
     }
 }
