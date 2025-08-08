0fcfc745670016c6d2a867d3280bb0535ccf60c5
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-08 14:09:44 -0400

    Initial commit - MiniCompiler

---
+++ src/Recursion.java
@@ -0,0 +1,32 @@
+public class Recursion {
+
+    public static int factorial(int n){
+        if( n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n * factorial(n-1); // function calling itself again with n-1 as it parameter
+        }
+    }
+    public static int iterativefactorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            int product = 1;
+            for(int i = 1; i <=n ; i++){
+                product*= i;
+
+            }
+            return product;
+        }
+
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("The factorial of number "+n+ " is: " + factorial(n));
+        System.out.println("The factorial of number "+n+ " is: " + iterativefactorial(n));
+
+    }
+}
