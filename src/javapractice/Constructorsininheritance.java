1be6280a0bc550dd9adff5e38d6a436f5b2636d9
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-20 22:46:08 -0400

    Initial commit - MiniCompiler

--- src/javapractice/Constructorsininheritance.java
+++ src/javapractice/Constructorsininheritance.java
@@ -3,8 +3,24 @@ package javapractice;
 class base1{
 
     public int x;
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
 
+}
 
+class base2 extends base1{
+    public int y;
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
 }
 public class Constructorsininheritance {
     public static void main(String[] args) {
