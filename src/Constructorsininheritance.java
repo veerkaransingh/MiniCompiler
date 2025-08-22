a0c6852adc46d5872629cdfc65af331f152054b1
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-22 17:28:03 -0400

    Initial commit - MiniCompiler

--- src/Constructorsininheritance.java
+++
@@ -1,6 +0,0 @@
-public class Constructorsininheritance {
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/javapractice/Constructorsininheritance.java
+++ src/javapractice/Constructorsininheritance.java
@@ -1,31 +1,30 @@
 package javapractice;
 
-class base1{
 
-    public int x;
-    public int getX(){
-        return x;
+class base1 {
+     base1() {
+        System.out.println("I am a constructor"); //constructor of base class would be called first that derived class
     }
-    public void setX(int x){
-        this.x = x;
+    base1(int a){
+        System.out.println("I am an overloaded constructor with value of a as: " + a);
     }
 
 }
+
 
 class base2 extends base1{
-    public int y;
-    public int getY(){
-        return y;
-    }
 
-    public void setY(int y){
-        this.y = y; //
-        //
-        //
-    }
+        base2(){
+            super(0); //if we want to run constructor having argument in base class, super keyword is used, if not, default will be called
+            System.out.println("i am a derived class constructor");
+        }
 }
+
 public class Constructorsininheritance {
     public static void main(String[] args) {
+        base2 b = new base2(); // we have initiated the object of derived or base2 class, now constructor of parent has been called first
+        // rather than the derived one
 
+        }
     }
-}
+
