d7b1f4e4861558d8252a1c48292720bc467bfbe1
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-23 18:59:32 -0400

    Initial commit - MiniCompiler

--- src/javapractice/Constructorsininheritance.java
+++ src/javapractice/Constructorsininheritance.java
@@ -2,11 +2,11 @@ package javapractice;
 
 
 class base1 {
-     base1() {
+    base1() {
         System.out.println("I am a constructor"); //constructor of base class would be called first that derived class
     }
-    base1(int a){
-        System.out.println("I am an overloaded constructor with value of a as: " + a);
+    base1(int x){
+        System.out.println("I am an overloaded constructor with value of x as: " + x);
     }
 
 }
@@ -14,16 +14,31 @@ class base1 {
 
 class base2 extends base1{
 
-        base2(){
+    base2(){
             super(0); //if we want to run constructor having argument in base class, super keyword is used, if not, default will be called
-            System.out.println("i am a derived class constructor");
-        }
+            System.out.println("I am a derived class constructor");
+    }
+    base2(int x, int y){
+            super(0);
+            System.out.println("I am an overlaoded constructor of derived or base2 with value of y as: "+y);
+    }
 }
 
+class ChildofBase2 extends base2{
+    ChildofBase2(){
+        System.out.println("I am a ChildofBase2 class constructor");
+    }
+    ChildofBase2(int x, int y, int z){
+        super(x,y);
+        System.out.println("I am an overloaded constructor of ChildofBase2 class with value of z as: "+z);
+    }
+
+}
 public class Constructorsininheritance {
     public static void main(String[] args) {
-        base2 b = new base2(); // we have initiated the object of derived or base2 class, now constructor of parent has been called first
-        // rather than the derived one
+        //base2 b = new base2(); // we have initiated the object of derived or base2 class, now constructor of parent has been called first
+        //rather than the derived one
+        ChildofBase2 bb = new ChildofBase2(2,3,4);
 
         }
     }
---
+++ src/javapractice/this_super.java
@@ -0,0 +1,27 @@
+package javapractice;
+
+
+class First{
+    int a;
+    First(){
+        System.out.println("I am the constructor");
+    }
+    public int getA(){
+        return a;
+    }
+    First(int a){
+        this.a = a;
+    }
+    public int reta(){
+        return 4;
+    }
+}
+
+class Second extends First{
+    Second(){
+        System.out.println("I am the constructor of derived class");
+    }
+}
+public class this_super {
+    //
+}
