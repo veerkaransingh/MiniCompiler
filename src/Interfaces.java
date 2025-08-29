b1db6c6895cb0ba516851ab445407079479c1e6c
Karan Singh <veerkaransinghmgm@gmail.com>
2025-08-29 19:49:54 -0400

    Initial commit - MiniCompiler

--- src/Interfaces.java
+++ src/Interfaces.java
@@ -1,8 +1,23 @@
-class B1{
 
+interface Bicycle{ //all the methods inside this are abstract, we have to implement them in any condition
+    //in java, group of related methods with empty bodies
+    void applyBrake(int decrement);
+    void speedUp(int increment);
 }
 
+class AvonCycle implements Bicycle{
+    int speed = 7;
+    public void applyBrake(int decrement) {
+        speed = speed - decrement;
+    }
+    public void speedUp(int increment){
+        speed = speed+increment;
+    }
+
+}
 public class Interfaces {
+    //INTERFACE IS A POINT WHERE TWO SYSTEMS INTERACT.
+    // IN JAVA, INTERFACE IS A GROUP OF RELATED METHODS WITH EMPTY BODIES
     public static void main(String[] args) {
         System.out.println("lets learn interfaces");
     }
