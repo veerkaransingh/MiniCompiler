d4b33e021f581cb0ee0fe7ae14c808806dd3927d
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-03 20:31:56 -0400

    Initial commit - Java Practice

--- src/javapractice/Packages.java
+++ src/javapractice/Packages.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Packages {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
         System.out.println("This is my scanner");
+
     }
 
 }
