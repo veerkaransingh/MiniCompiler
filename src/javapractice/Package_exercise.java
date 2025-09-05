40fc40c63910e08c89c0e57eaed189d623274d15
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-05 17:33:39 -0400

    Initial commit - Java Practice

---
+++ src/javapractice/Package_exercise.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class Package_exercise {
+
+    /* you have to create a package named com.codewithharry.shape
+    This package should have individual classes for Rectangle, Square, Circle, Cylinder, Sphere,
+    These classes should use inheritance to properly manage the code!
+    Include methods like volume, surface area and getters/setters for dimensions
+     */
+
+}
