63665e9effb68e2b0c4780488c32323340db6ba5
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-04 20:52:11 -0400

    Initial commit - Java Practice

---
+++ src/javapractice/Access_modifiers.java
@@ -0,0 +1,15 @@
+package javapractice;
+
+
+class C1{
+    public int x = 5; // can be used in same class,
+    protected int y = 45;
+    int z = 6;
+    private int a = 76;
+}
+public class Access_modifiers{
+    public static void main(String[] args) {
+
+    }
+
+}
