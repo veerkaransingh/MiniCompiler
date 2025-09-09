7051a63544afd61b2d6b24a317bfa96a1514157f
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-08 20:24:41 -0400

    Initial commit - Java Practice

---
+++ src/Exception_handling/Exercise_CALCULATOR.java
@@ -0,0 +1,19 @@
+package Exception_handling;
+
+public class Exercise_CALCULATOR {
+    public static void main(String[] args) {
+        /* you have to create a custom calculator which does the following operations
+        and throws the following exceptions
+        1. + --> Addition
+        2. - --> Subtraction
+        3. * --> Multiplication
+        4. / --> Division
+        which throws the following exceptions:
+        1. Invalid input Exception ex : 8 & 9
+        2. Cannot divide by 0 Exception
+        3. Max Input Exception -- if any of the input is greater than 100000
+        4. Can't multiply by any number greater than 7000,,,, Multiplier Reached Exception
+
+         */
+    }
+}
