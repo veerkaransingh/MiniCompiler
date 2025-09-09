1dbb12c9c3caa20ef30eba1af7e72731b574c149
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-08 20:24:28 -0400

    Initial commit - Java Practice

---
+++ src/Collections/Collections.java
@@ -0,0 +1,14 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Collections {
+    // Java FX (GUI), Java SE (General purpose Computing), Java EE jakarta EE (Large scale enterprise
+    // applications), Java card ( run programs on small memory chips) , Java ME( Designed for mobile phones, embedded devices)
+
+
+    // Group of objects(data)
+    public static void main(String[] args) {
+    }
+}
