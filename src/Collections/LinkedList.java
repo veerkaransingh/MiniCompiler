646913f8dd494f1390274902b36af4972b13561a
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-09 21:24:22 -0400

    Initial commit - Java Practice

---
+++ src/Collections/LinkedList.java
@@ -0,0 +1,11 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Lets learn about linked list!!!!");
+        // array list
+        ArrayList<Integer> list1 = new ArrayList<>(1000);
+    }
+}
