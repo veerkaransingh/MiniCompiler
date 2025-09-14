4aea970ac539e30dded8d0d7675d580149b78724
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-14 12:26:14 -0400

    Initial commit - Java Practice

---
+++ src/Java_Annotations/Java_annotations.java
@@ -0,0 +1,17 @@
+package Java_Annotations;
+
+@FunctionalInterface  //used to ensure an interface is an functional interface
+ interface myFunctionalInterface{
+    void thisMethod();
+}
+public class Java_annotations {
+    // tells extra information about the program
+    // provides metadata -- information about some information
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        // @Override - when we override any method
+        // @SuppressWarnings - used to suppress the generated warnings by the compiler
+        //m
+    }
+
+}
